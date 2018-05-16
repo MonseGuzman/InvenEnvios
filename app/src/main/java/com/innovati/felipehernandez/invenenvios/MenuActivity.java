ddfc450b35eaffad02a179bc9bf6dff6685dcfae
Monse Guzmaán <andreaguzman.lopez23@gmail.com>
2018-05-15 21:06:59 -0500

    miren diseño carita oww

--- app/src/main/java/com/innovati/felipehernandez/invenenvios/MenuActivity.java
+++ app/src/main/java/com/innovati/felipehernandez/invenenvios/MenuActivity.java
@@ -9,5 +9,7 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        this.setTitle("Menú");
     }
 }
