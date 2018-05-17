2f579de04fd416f77f8063777efa195f20019037
Monse Guzmaán <andreaguzman.lopez23@gmail.com>
2018-05-16 20:02:53 -0500

    adaptador articulos

--- app/src/main/java/com/innovati/felipehernandez/invenenvios/MenuActivity.java
+++ app/src/main/java/com/innovati/felipehernandez/invenenvios/MenuActivity.java
@@ -5,7 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
