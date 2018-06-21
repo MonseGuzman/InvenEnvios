6c9aa4323cf1bb7adb484a27823c4dfbc9ff984f
Unknown <rootn66@gmail.com>
2018-06-21 16:40:11 -0500

    parte del diseño del carrito
    se añadio el diseño y la activity del carrito

---
+++ app/src/main/java/com/innovati/felipehernandez/invenenvios/activitys/ArticulosPedidosActivity.java
@@ -0,0 +1,13 @@
+package com.innovati.felipehernandez.invenenvios.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArticulosPedidosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_articulos_pedidos);
+    }
+}
