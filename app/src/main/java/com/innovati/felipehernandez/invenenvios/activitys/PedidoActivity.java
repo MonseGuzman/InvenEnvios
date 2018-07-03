5dfb2f8085e8681676f54d4d9d08eed547a3a660
diego0309 <diegoosvaldo0309@gmail.com>
2018-07-03 18:13:24 -0500

    YA sirve el folio x2

--- app/src/main/java/com/innovati/felipehernandez/invenenvios/activitys/PedidoActivity.java
+++ app/src/main/java/com/innovati/felipehernandez/invenenvios/activitys/PedidoActivity.java
@@ -303,11 +303,11 @@ public class PedidoActivity extends AppCompatActivity
 
     public void geneFolio(){
         String  folioAux = "";
-        //"%05d%n", 5
+        //"%05d%n", 5ffff
         Pedidos pedidosResult[] = null;
         PedidosDao daoPedidos = getPedidosDao();
         try{
-            pedidosResult = daoPedidos.findByDynamicSelect("SELECT Folio FROM Pedidos", null);
+            pedidosResult = daoPedidos.findByDynamicSelect("SELECT NULL, NULL, Folio, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL FROM Pedidos", null);
         }catch (Exception e){}
 
         folioAux = String.format("%05d%n",pedidosResult.length+1);
