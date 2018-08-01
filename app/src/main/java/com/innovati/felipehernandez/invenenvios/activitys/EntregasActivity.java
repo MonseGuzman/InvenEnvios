e3a4ab497e9357ee9bf2cf584ad12d7c1f297009
Unknown <rootn66@gmail.com>
2018-07-31 19:40:38 -0500

    añadir el adapter de de entregas

--- app/src/main/java/com/innovati/felipehernandez/invenenvios/activitys/EntregasActivity.java
+++ app/src/main/java/com/innovati/felipehernandez/invenenvios/activitys/EntregasActivity.java
@@ -7,6 +7,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,7 +18,9 @@ import android.widget.ListView;
 
 import com.innovati.felipehernandez.invenenvios.MetodosInternos;
 import com.innovati.felipehernandez.invenenvios.R;
+import com.innovati.felipehernandez.invenenvios.adapters.EntregasRecycleView;
 import com.innovati.felipehernandez.invenenvios.adapters.PedidosAdapter;
+import com.innovati.felipehernandez.invenenvios.adapters.RecycleViewOnItemClickListener;
 import com.innovati.felipehernandez.invenenvios.app.MyApp;
 import com.innovati.felipehernandez.invenenvios.clases.dao.PedidosDao;
 import com.innovati.felipehernandez.invenenvios.clases.dao.VwPedidosDao;
@@ -35,13 +40,14 @@ import java.util.List;
 
 public class EntregasActivity extends AppCompatActivity
 {
-    private ListView EntregasListView;
+    //private ListView EntregasListView;
 
     private PedidosAdapter adaptador;
     private VwPedidos result[];
     private MetodosInternos metodosInternos = new MetodosInternos(this);
     private DaoSession daoSession;
     private ProgressDialog dialog;
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +58,7 @@ public class EntregasActivity extends AppCompatActivity
 
         daoSession = ((MyApp) getApplication()).getDaoSession();
 
-        EntregasListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*EntregasListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
             {
@@ -67,9 +73,9 @@ public class EntregasActivity extends AppCompatActivity
                 getSupportFragmentManager().beginTransaction().replace(R.id.EntregasRelativeLayout, datosPedidoFragment).addToBackStack(null).commit();
 
             }
-        });
+        });*/
 
-        EntregasListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        /*EntregasListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id)
             {
@@ -102,17 +108,91 @@ public class EntregasActivity extends AppCompatActivity
 
                 return false;
             }
-        });
-
+        });*/
         cargarDatos();
+
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int  position = viewHolder.getAdapterPosition();
+                if (direction == ItemTouchHelper.RIGHT){
+                    SharedPreferences preferences = getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+
+                    Pedidos pedidos = new Pedidos();
+
+                    if(result[position].getEstatus() == 3){
+                        result[position].setEstatus((short) 4);
+                    }else{
+                        result[position].setEstatus((short) 3);
+                    }
+                    pedidos.setIdPedido(result[position].getIdPedido());
+                    pedidos.setIdUsuario(result[position].getIdUsuario());
+                    pedidos.setFolio(result[position].getFolio());
+                    pedidos.setClaveCliente(result[position].getClaveCliente());
+                    pedidos.setFecha(result[position].getFecha());
+                    pedidos.setEstatus(result[position].getEstatus());
+                    pedidos.setSubtotal(result[position].getSubtotal());
+                    pedidos.setIva(result[position].getIva());
+                    pedidos.setTotal(result[position].getTotal());
+
+                    pedidos.setUltimaFechaActualizacion( new Date());
+                    pedidos.setUltimoUsuarioActualizacion(preferences.getString("idUsuario", ""));
+                    ActualizarPedido a = new ActualizarPedido();
+                    a.execute(pedidos);
+
+                }else if (direction == ItemTouchHelper.LEFT){
+                    /*ArticulosPedidosAdapter adapter = (ArticulosPedidosAdapter) recyclerEntregas.getAdapter();
+                    ArticulosPedido articulosPedido = new ArticulosPedido();
+                    articulosPedido = adapter.articulosPedidos.get(position);
+                    if(articulosPedido.isStatus()){
+                        articulosPedido.setStatus(false);
+                    }else{
+                        articulosPedido.setStatus(true);
+                    }
+                    adapter.articulosPedidos.set(position,articulosPedido);*/
+                }
+                updateAdapter();
+            }
+
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
     }
 
     private void inicializacion()
     {
-        EntregasListView = (ListView)findViewById(R.id.EntregasListView);
+        //EntregasListView = (ListView)findViewById(R.id.EntregasListView);
         dialog=new ProgressDialog(this);
         dialog.setMessage("Cargando...");
         dialog.setCancelable(false);
+        recyclerView =(RecyclerView) findViewById(R.id.recycleViewEntrega);LinearLayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setHasFixedSize(true);
+
+    }
+
+    private void updateAdapter(){
+        recyclerView.setAdapter(new EntregasRecycleView(this, result , new RecycleViewOnItemClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                /*DetallePedidoFragment datosPedidoFragment = new DetallePedidoFragment();
+                Bundle args;
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_regresar);
+                args = new Bundle();
+                args.putString("pedido", result[position].getIdPedido());
+                args.putBoolean("bandera",false);
+                datosPedidoFragment.setArguments(args);
+                getSupportFragmentManager().beginTransaction().replace(R.id.EntregasRelativeLayout, datosPedidoFragment).addToBackStack(null).commit();
+*/
+            }
+        }));
     }
 
     @Override
@@ -206,9 +286,9 @@ public class EntregasActivity extends AppCompatActivity
             objetoPedidos.setIva(pedidos.get(x).getIva());
             result[x] = objetoPedidos;
         }
-
-        adaptador = new PedidosAdapter(EntregasActivity.this,  R.layout.listview_pedidos, result, 2);
-        EntregasListView.setAdapter(adaptador);
+        updateAdapter();
+        /*adaptador = new PedidosAdapter(EntregasActivity.this,  R.layout.listview_pedidos, result, 2);
+        EntregasListView.setAdapter(adaptador);*/
     }
 
     private class ConsultaPedidos extends AsyncTask<VwPedidosDao,Void, VwPedidos[]>
@@ -230,9 +310,9 @@ public class EntregasActivity extends AppCompatActivity
         @Override
         protected void onPostExecute(VwPedidos[] pedidos) {
             super.onPostExecute(pedidos);
-
-            adaptador = new PedidosAdapter(EntregasActivity.this,  R.layout.listview_pedidos, result, 2);
-            EntregasListView.setAdapter(adaptador);
+            updateAdapter();
+            /*adaptador = new PedidosAdapter(EntregasActivity.this,  R.layout.listview_pedidos, result, 2);
+            EntregasListView.setAdapter(adaptador);*/
         }
     }
 
