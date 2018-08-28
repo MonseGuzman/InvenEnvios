919c99b7f6326ce0c120eacc90001a09ee14c49d
diego0309 <diegoosvaldo0309@gmail.com>
2018-08-28 17:01:54 -0500

    arreglados errores de db interna

--- app/src/main/java/com/innovati/felipehernandez/invenenvios/activitys/AbastecimientosActivity.java
+++ app/src/main/java/com/innovati/felipehernandez/invenenvios/activitys/AbastecimientosActivity.java
@@ -330,7 +330,7 @@ public class AbastecimientosActivity extends AppCompatActivity implements Adapte
                 {
 
                     String nombre = abastecimiento.getNombre() + " " + abastecimiento.getTotal() + abastecimiento.getUnidadPrimaria();
-                    VwDetallePedido[] detalles = _daoDetalle.findByDynamicSelect("SELECT NULL, NULL, NULL, NULL, Cantidad, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL FROM VwDetallePedido WHERE Surtido = ? AND Nombre = ?",new Object[]{0,abastecimiento.getNombre()});
+                    VwDetallePedido[] detalles = _daoDetalle.findByDynamicSelect("SELECT NULL, NULL, NULL, NULL, Cantidad, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL FROM VwDetallePedido WHERE Surtido = ? AND Nombre = ?",new Object[]{1,abastecimiento.getNombre()});
 
                     List<Float> numeros = new ArrayList<>();
                     for(VwDetallePedido cantidades: detalles)
